cfe653089d1798bd8badb16004f55dbe6cea6671
CihHuei <pie8628@gmail.com>
2022-03-19 23:33:15 +0800

    0319

--- js/all.js
+++ js/all.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   $(window).scroll(function () {
-    last=$("body").height()-$(window).height()-1500
+    last=$("body").height()-$(window).height()-3500
     if($(window).scrollTop()>=last){
       $("#socialVdo")[0].play();
       $("#socialVdo2")[0].play();
